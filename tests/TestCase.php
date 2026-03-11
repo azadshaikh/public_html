5df106a12297553abb9b1d964b5597a5e0542d59
Azad Shaikh <mohd.azad.shaikh@gmail.com>
2026-03-11 19:29:20 +0530

    feat: implement setup method in TestCase to configure ModuleManager and modules manifest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,25 @@
 
 namespace Tests;
 
+use App\Modules\ModuleManager;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Laravel\Fortify\Features;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config()->set('modules.manifest', base_path('modules.json'));
+
+        app()->forgetInstance(ModuleManager::class);
+        app()->singleton(ModuleManager::class, fn ($app): ModuleManager => new ModuleManager(
+            files: $app['files'],
+            config: $app['config'],
+        ));
+    }
+
     protected function skipUnlessFortifyFeature(string $feature, ?string $message = null): void
     {
         if (! Features::enabled($feature)) {
